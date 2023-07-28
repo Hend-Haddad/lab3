e02477e7dee72944f0702ff00c51f877a74ae1a6
Hend Haddad <hendhadad2000@gmail.com>
2023-07-28 14:53:45 +0200

    add  a function to create post

--- User.php
+++ User.php
@@ -1,8 +1,15 @@
 <?php
+require_once './Post.php';
 class  User
 {
     public function __construct(
         public string $user
     ) {
+    }
+
+    public function post(string $content): Post
+    {
+        $post = new post($content);
+        return $post;
     }
 }
