51860b302dd1da14bce6d7d432e5ae5d53f50a55
Hend Haddad <hendhadad2000@gmail.com>
2023-07-28 09:28:58 +0200

    added class Post and their fonctionality

---
+++ Comment.php
@@ -0,0 +1,9 @@
+<?php
+class Comment
+
+{
+    public function __construct(
+        public string $comment
+    ) {
+    }
+}
---
+++ Post.php
@@ -0,0 +1,35 @@
+<?php
+require_once './Comment.php';
+require_once './User.php';
+class Post
+{
+    /**
+     * @var Comment
+     */
+    private $comment = [];
+    public function __construct(
+        public string $post
+    ) {
+    }
+
+    public function like(): void
+    {
+    }
+    public function addComment(Comment $comment): void
+    {
+        $comment1 = new Comment('$comment1');
+        echo $comment1;
+    }
+    public function getComment(): array
+    {
+        return var_dump([$this->comment]);
+    }
+    public function isLikedBy(User $user): bool
+    {
+        $boolean = false;
+        if ($user->like() = 1) {
+            $boolean = true;
+        }
+        return $boolean;
+    }
+}
